335614f8bca520636b132a192ad056b4eaac5b0d
fajarkarim <fajarabdulkarim@gmail.com>
2017-08-08 19:12:01 +0700

    added seed and clear

--- server/controllers/articlesController.js
+++ server/controllers/articlesController.js
@@ -13,7 +13,13 @@ var getAll = (req, res) => {
 }
 
 var getOne = (req, res) => {
-  res.send('getOne')
+  Article.findById(req.params.id)
+  .then(found => {
+    res.send(found)
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
 }
 
 var create = (req, res) => {
@@ -36,15 +42,23 @@ var seed = (req, res) => {
     {
       author: 'fajar',
       title: '5 cara hidup sehat',
-      content: 'ini adalah salah satu contoh konten'
+      content: `Udara bersih, paru-paru pun sehat
+Hiruplah udara yang bersih dan sehat, maka Anda terhindar dari gangguan pernapasan. Udara pagi adalah sangat baik untuk paru-paru Anda, tanpa repot mencari udara pegunungan. Sempatkanlah diri keluar rumah sebelum berangkat beraktifitas untuk mengatur pernapasan menghirup udara sejuk. Ketika berada di luar, sebisa mungkin menjauhlah dari udara tercemar seperti asap kendaraan atau debu dan asap rokok. Perabot rumah, kipas angin atau AC di rumah dan ruangan kerja Anda juga jangan lupa untuk rutin dibersihkan.
+Banyak minum air putih
+
+Dibanding minuman apapun, air putih lah yang terbaik. Seperti anjuran pada umumnya, per hari minimal minumlah air putih 8-9 gelas. Fungsi ginjal dan saluran kemih pun terbantu kelancarannya dengan meminum air putih. Di siang hari, minumlah air sejuk (bukan air es) dan air hangat di malam hari. Anda juga bisa menambahkan air jeruk lemon atau jeruk nipis. Minuman ini berguna mengeluarkan toksin dari dalam tubuh, selain menyegarkan diri.`
     },{
       author: 'dzaki',
       title: 'ekonomi syariah',
-      content: 'kontent syariah'
+      content: `Krisis ekonomi yang sering terjadi ditengarai adalah ulah sistem ekonomi konvensional, yang mengedepankan sistem bunga sebagai instrumen profitnya. Berbeda dengan apa yang ditawarkan sistem ekonomi syariah, dengan instrumen profitnya, yaitu sistem bagi hasil.
+
+Sistem ekonomi syariah sangat berbeda dengan ekonomi kapitalis, sosialis maupun komunis. Ekonomi syariah bukan pula berada di tengah-tengah ketiga sistem ekonomi itu. Sangat bertolak belakang dengan kapitalis yang lebih bersifat individual, sosialis yang memberikan hampir semua tanggung jawab kepada warganya serta komunis yang ekstrim[1], ekonomi Islam menetapkan bentuk perdagangan serta perkhidmatan yang boleh dan tidak boleh di transaksikan[4]. Ekonomi dalam Islam harus mampu memberikan kesejahteraan bagi seluruh masyarakat, memberikan rasa adil, kebersamaan dan kekeluargaan serta mampu memberikan kesempatan seluas-luasnya kepada setiap pelaku usaha.`
     },{
       author: 'rifma',
       title: 'psikologi sehat',
-      content: 'cara menuju psikologi sehat'
+      content: `Pengalaman awal merupakan segenap pengalaman-pengalaman yang terjadi pada individu terutama yang terjadi di masa lalunya. Pengalaman awal ini adalah merupakan bagian penting dan bahkan sangat menentukan bagi kondisi mental individu di kemudian hari.Kebutuhan Pemenuhan kebutuhan dapat meningkatkan kesehatan mental seseorang. Orang yang telah mencapai kebutuhan aktualisasi yaitu orang yang mengeksploitasi dan segenap kemampuan bakat, ketrampilannya sepenuhnya, akan mencapai tingkatan apa yang disebut dengan tingkatan pengalaman puncak.Dalam berbagai penelitian ditemukan bahwa orang-orang yang mengalami gangguan mental, disebabkan oleh ketidakmampuan individu memenuhi kebutuhan-kebutuhannya. Kebutuhan yang dimaksud di sini adalah kebutuhan dasar yang tersusun secara hirarki. Kebutuhan biologis, kebutuhan rasa aman, meliputi kebutuhan dicintai, kebutuhan harga diri, pengetahuan, keindahan dan kebutuhan aktualisasi diri.
+
+Read more: http://doktersehat.com/psikologi-kesehatan-mental/#ixzz4p9yMkXSo`
     }
   ]
   Article.insertMany(articles)
